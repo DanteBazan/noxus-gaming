dddbcf9180a216e6f3a6962618a180555599eff9
Dante <bazandante05@gmail.com>
2024-05-25 14:41:36 -0300

    fift commit

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,10 @@ const openMenu = document.querySelector(".open__menu");
 const navBarList = document.querySelector(".navbar__list");
 // boton para cerrar
 const closeMenu = document.querySelector(".close__menu");
+// boton del carrito
+const buttonCart = document.querySelector(".cart");
+// contenedor de cart
+const cartContainer = document.querySelector(".container__cart");
 
 // funcion para mostrar menu
 const viewMenu = () => {
@@ -15,10 +19,15 @@ const hideMenu = () => {
   navBarList.classList.remove("visibilite");
 };
 
+const showCart = () => {
+  cartContainer.classList.toggle("show__cart");
+};
+
 // funcion inicializadora
 const init = () => {
   openMenu.addEventListener("click", viewMenu);
   closeMenu.addEventListener("click", hideMenu);
+  buttonCart.addEventListener("click", showCart);
 };
 
 init();
